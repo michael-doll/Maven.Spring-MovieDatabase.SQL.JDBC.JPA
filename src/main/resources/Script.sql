5f9c433b31f3ab3f7f6459dd6f04975fcbfa714d
michael-doll <MWDoll88@gmail.com>
2019-12-06 18:51:32 -0500

    Create movie table

---
+++ src/main/resources/Script.sql
@@ -0,0 +1,58 @@
+/*Add the following movies to the movies table using an insert statement:*/
+DROP TABLE IF EXISTS TEST;
+CREATE TABLE Movies(
+                       Title varchar(255),
+                       Runtime int,
+                       Genre varchar(255),
+                       IMDBScore int,
+                       Rating varchar(255)
+);
+
+INSERT INTO movies(
+    Title,
+    Runtime,
+    Genre,
+    IMDBScore,
+    Rating)
+Values(
+          'Howard The Duck',
+          110,
+          'Sci-Fi',
+          4.6,
+          'PG'),
+      (
+          'Lavalantula',
+          83,
+          'Horror',
+          4.7,
+          'TV-14'),
+      (
+          'Starship Troopers',
+          129,
+          'Sci-Fi',
+          7.2,
+          'PG-13'
+      ),
+      (
+          'Waltz With Bashir',
+          83,
+          'Documentary',
+          8.0,
+          'R'
+      ),
+      (
+          'Spaceballs',
+          96,
+          'Comedy',
+          7.1,
+          'PG'
+      ),
+      (
+          'Monsters Inc.',
+          92,
+          'Animation',
+          8.1,
+          'G'
+      );
+
+
